6ed27610a636d1d0247ec32de6aa2d6ea9528f31
gh-huang <gh_huang@outlook.com>
2016-08-18 16:51:57 +0800

    learn console

--- app/Console/Commands/HelloLaravelAcademy.php
+++ app/Console/Commands/HelloLaravelAcademy.php
@@ -37,12 +37,36 @@ class HelloLaravelAcademy extends Command
      */
     public function handle()
     {
-        $name = $this->argument('name');
-        $mark = $this->option('mark');
-        $string = 'Hello ' . $name;
-        if ($mark) {
-            $string .= $mark;
+        // $name = $this->argument('name');
+        // $mark = $this->option('mark');
+        // $string = 'Hello ' . $name;
+        // if ($mark) {
+        //     $string .= $mark;
+        // }
+        // echo $string . "\n";
+        // $name = $this->ask('What do you want to say Hello?');
+        // echo "Hello " . $name . "\n";
+        // if ($this->confirm('Do you want to continue?[y|n]')) {
+        //     $this->info('Continue');
+        // } else {
+        //     $this->error('Interput');
+        // }
+        // $name = $this->anticipate('What is your name?', ['Laravel', 'World']);
+        // $this->info($name);
+        // $name = $this->choice('What is your name?', ['Laravel', 'World']);
+        // $this->info($name);
+        // $this->info('Successful');
+        // $this->error('Something Error');
+        // $this->question('What do you want to do');
+        // $this->comment('Just Comment it!');
+        // $headers = ['Name', 'Email'];
+        // $users = \App\Models\User::all(['name', 'email'])->toArray();
+        // $this->table($headers, $users);
+        $this->output->progressStart(10);
+        for ($i = 0; $i < 10; $i++) { 
+            sleep(1);
+            $this->output->progressAdvance();
         }
-        echo $string . "\n";
+        $this->output->progressFinish();
     }
 }
