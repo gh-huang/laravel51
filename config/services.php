a2ff1872a91db5deea42b3b69a6215b890611d98
gh-huang <gh_huang@outlook.com>
2016-08-18 22:25:24 +0800

    Socialite github

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://laravel51.com/auth/github/callback',
+        'redirect' => 'http://www.laravel51.com/auth/github/callback',
     ],
 
 ];
