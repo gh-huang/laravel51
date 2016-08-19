eace9e526822727a4d811672def0c1c5d73da658
gh-huang <gh_huang@outlook.com>
2016-08-19 22:19:39 +0800

    migrate

--- database/migrations/2016_08_19_213108_add_github_id_and_avator.php
+++ database/migrations/2016_08_19_213108_add_github_id_and_avator.php
@@ -13,7 +13,8 @@ class AddGithubIdAndAvator extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('github_id');
+            $table->string('avatar');
         });
     }
 
