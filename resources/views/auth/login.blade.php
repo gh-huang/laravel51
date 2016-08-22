5a2a424bf924c6a3702afd36336069bcd3a0f83d
gh-huang <gh_huang@outlook.com>
2016-08-22 12:20:05 +0800

    local lang

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-danger">
         <ul style="color: red;">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li>{{ trans($error) }}</li>
             @endforeach
         </ul>
     </div>
