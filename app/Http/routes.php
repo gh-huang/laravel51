35164ed0f388f25f5b704e1fdf68f6bf12b8be62
gh-huang <gh_huang@outlook.com>
2017-01-03 20:13:39 +0800

    ar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -615,7 +615,7 @@ Route::get('happy', function () {
 	echo "happy";
 	echo "new";
 	echo "year";
-	echo "happy new ye";
+	echo "happy new year";
 });
 // auth route
 Route::group(['namespace' => 'auth'], function () {
